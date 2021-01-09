9670e6dfeb920560deb773cb96dc24e8106ec5db
Kathryn Kane <Kathryn@Kathryns-MacBook-Pro-2.local>
2021-01-08 23:32:49 -0800

    calculated votes per candidate and winner

--- PyPoll.py
+++ PyPoll.py
@@ -53,10 +53,55 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+#initialize a total vote counter
+total_votes = 0
+
+#candidate options
+candidate_options = []
+#declare empty dictionary
+candidate_votes = {}
+#winnin candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     #Read and Print the header row. 
     headers = next(file_reader)
-    print(headers)
+    
+    #print each row in the CSV file
+    for row in file_reader:
+        #add to the total vote count
+        total_votes += 1
+        #Print the candidate name from each row
+        candidate_name = row[2]
+        #If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            #Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+            #Begin tracking that candidate's vote
+            candidate_votes[candidate_name] = 0
+        #Add a vote to that candidate's vote count
+        candidate_votes[candidate_name]+= 1
+for candidate_name in candidate_votes:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    if(votes>winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+    #Print the winning candidate, vote count, and percentage
+    print(f"{candidate_name}: {vote_percentage:.1f}%({votes:,})\n")
+
+winning_candidate_summary = (
+    f"---------------------\n"
+    f"winner: {winning_candidate}\n"
+    f"winning vote count: {winning_count:,}\n"
+    f"----------------------------------------\n")
+print(winning_candidate_summary)
+
+
+
 
